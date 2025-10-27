ff5573c6f3e249c9d277e6bd3ddaf12555ad7be6
GeronimoAhil <geromy2014@gmail.com>
2025-10-27 16:04:45 +0800

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,24 @@
+name = "tiger"
+age = 5
+is_student = True
+weight = 15.75
+
+print("name:",name)
+print("data type of name:",type(name))
+
+print("age:",age)
+print("data type of age:",type(age))
+
+print("is_student:",is_student)
+print("data type of is_student:",type(is_student))
+
+print("weight:",weight)
+print("data type of weight:",type(weight))
+print("\ntypecasting")
+age = str(age)
+print("age:",age)
+print("data type of age:",type(age))
+weight = int(weight)
+print("weight:",weight)
+print("data type of weight:",type(weight))
+
